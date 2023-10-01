4958f14e14256157a80c5ac361d2fc0fa147c71e
Anything-bro <134754968+Anything-bro@users.noreply.github.com>
2023-10-01 12:56:07 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,4 +1,10 @@
-git clone https://github.com/Anything-bro/Z-Mirror
-cd 
-pip3 install -U -r requirements.txt
-bash start.sh
+FROM nikolaik/python-nodejs:python3.9-nodejs20
+RUN apt-get update -y && apt-get upgrade -y \
+    && apt-get install -y --no-install-recommends ffmpeg \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+COPY . /app/
+WORKDIR /app/
+RUN git clone https://github.com/Anything-bro/Z-Mirror
+RUN cd Z-Mirror && pip3 install -U -r requirements.txt
+CMD cd Z-Mirror && python3 update.py && python3 -m bot
